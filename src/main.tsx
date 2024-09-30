794742351c8800153fbc1fdd4b2c242f12edae45
eggzuxi <eggzuxi99@gmail.com>
2024-09-30 13:55:28 +0900

    addLayout_JH

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import './satoshi.css';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
